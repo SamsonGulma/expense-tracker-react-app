8f11e48cf3e22cbfeb998fb2e5393dd39a99fdbb
SamsonGulma <sams0ngultek@gmail.com>
2024-05-08 20:35:21 +0000

    May 8, 2024, 11:35 PM

--- expense-tracker-budgeting-app/src/App.jsx
+++ expense-tracker-budgeting-app/src/App.jsx
@@ -1,10 +1,22 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <root />,
+  },
+]);
+
 function App() {
 
-  return (
-    <>
-        
-    </>
-  )
+  return <div className="App"> 
+      <RouterProvider router={router} />
+  </div>
+    
+  
 }
 
 export default App
